d64d278dfadb47976514f54d1b914379000ec6a6
Andrew Kanieski <ankan@microsoft.com>
2017-09-06 19:27:01 -0400

    Added more help text

--- lib/caravan.ts
+++ lib/caravan.ts
@@ -1,14 +1,23 @@
 import * as processArgs from 'command-line-args'
+import * as getUsage from 'command-line-usage'
 import { IMigrator, IMigratorOptions } from './interfaces'
 import { MsSqlMigrator } from './migrators/mssql'
 
+const title = ` 
+ ,adPPYba, ,adPPYYba, 8b,dPPYba, ,adPPYYba, 8b       d8 ,adPPYYba, 8b,dPPYba,    
+a8"     "" ""     'Y8 88P'   "Y8 ""     'Y8 '8b     d8' ""     'Y8 88P'   '"8a   
+8b         ,adPPPPP88 88         ,adPPPPP88  '8b   d8'  ,adPPPPP88 88       88   
+"8a,   ,aa 88,    ,88 88         88,    ,88   '8b,d8'   88,    ,88 88       88   
+ '"Ybbd8"' '"8bbdP"Y8 88         '"8bbdP"Y8     "8"     '"8bbdP"Y8 88       88  
+`
 
 const optionDefinitions = [
-    { name: 'connection', alias: 'c', type: String },
-    { name: 'schema', alias: 's', type: String},
-    { name: 'table', alias: 't', type: String},
-    { name: 'debug', alias: 'd', type: Boolean},
-    { name: 'version', alias: 'v', type: Boolean}
+    { name: 'connection', alias: 'c', type: String, description: 'Database connection string in URI format' },
+    { name: 'schema', alias: 's', type: String, description: '[Optional] Database schema for migrations'},
+    { name: 'table', alias: 't', type: String, description: '[Optional] Table name for migrations'},
+    { name: 'debug', alias: 'd', type: Boolean, description: '[Optional] Debug Mode'},
+    { name: 'version', alias: 'v', type: Boolean, description: '[Optional] Version Information'},
+    { name: 'help', alias: 'h', type: Boolean, description: 'Help'}
 ]
 
 
@@ -16,17 +25,22 @@ let args = processArgs(optionDefinitions)
 global['debug'] = args.debug
 
 if (args.version) {
+    console.log(title + '\n')
     console.log('Caravan DB v' + require('../package').version)
     process.exit(0)
 }
+if (args.help) {
+    console.log(title)
+    console.log(getUsage([
+        {
+            header: 'Options',
+            optionList: optionDefinitions
+        }
+    ]))
+    process.exit(0)
+}
 
-console.log(` 
- ,adPPYba, ,adPPYYba, 8b,dPPYba, ,adPPYYba, 8b       d8 ,adPPYYba, 8b,dPPYba,    
-a8"     "" ""     'Y8 88P'   "Y8 ""     'Y8 '8b     d8' ""     'Y8 88P'   '"8a   
-8b         ,adPPPPP88 88         ,adPPPPP88  '8b   d8'  ,adPPPPP88 88       88   
-"8a,   ,aa 88,    ,88 88         88,    ,88   '8b,d8'   88,    ,88 88       88   
- '"Ybbd8"' '"8bbdP"Y8 88         '"8bbdP"Y8     "8"     '"8bbdP"Y8 88       88  
-`)
+console.log(title)
 
 import {DEBUG, INFO, ERROR} from './utils'
 
